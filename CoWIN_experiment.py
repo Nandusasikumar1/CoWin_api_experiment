4b816c38cb80b68774dc67c30557e685de312d93
Nandu  s <74356710+Nandusasikumar1@users.noreply.github.com>
2021-06-02 10:48:36 +0530

    Update CoWIN_experiment.py

--- CoWIN_experiment.py
+++ CoWIN_experiment.py
@@ -15,11 +15,6 @@ def cowin_data():
     response=requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?',params={'district_id':str(dist_id),'date':date})
     return response
 def data_frame():
-    response=cowin_data()
-    c=[]
-    for i in response.json():
-        for k in response.json()[i]:
-            c.append(k)
     st.set_page_config(page_title="Nandu's CoWin api experiment")
     hide_streamlit_style = """
     <style>
@@ -28,5 +23,11 @@ def data_frame():
     </style>
     """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    response=cowin_data()
+    c=[]
+    for i in response.json():
+        for k in response.json()[i]:
+            c.append(k)
+    
     st.dataframe(c)
 data_frame()

e6ddbad5764b1be3f021f84af874494723eed418
Nandu  s <74356710+Nandusasikumar1@users.noreply.github.com>
2021-06-02 10:34:34 +0530

    Update CoWIN_experiment.py

--- CoWIN_experiment.py
+++ CoWIN_experiment.py
@@ -31,5 +31,5 @@ def show():
     </style>
     """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    st.dataframe(data_frame(),width=2000,height=1500)
+    st.write(st.dataframe(data_frame()))
 show()

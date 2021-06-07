0084e699aa79e45c627296d75782154db8ad0887
Nandu  s <74356710+Nandusasikumar1@users.noreply.github.com>
2021-06-07 15:33:47 +0530

    Update CoWIN_experiment.py

--- CoWIN_experiment.py
+++ CoWIN_experiment.py
@@ -14,14 +14,22 @@ class cowin():
         districts=st.selectbox('Select District',list(file['districts']))
         dist_id=list(file[file['districts']==districts]['district_id'])[0]
         response=requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?',params={'district_id':str(dist_id),'date':date})
-        return response
-    def data_frame(self):
-        response=self.cowin_data()
         c=[]
-        for i in response.json():
-            for k in response.json()[i]:
-                c.append(k)
-        return c
+        try:
+            for i in response.json():
+                for k in response.json()[i]:
+                    c.append(k)
+            keyss=[i.keys() for i in c]
+            keys1=list(np.unique(keyss)[0])
+            index=[keys1.index(i) for i in ['name','pincode','fee_type','min_age_limit','vaccine','available_capacity_dose1','available_capacity_dose2']]
+            data={}
+            for f in index:
+                data[keys1[f]]=[]
+                for p in c:
+                    data[keys1[f]].append(list(p.values())[f])
+            return data
+        except:
+            st.write('No center available')
 
     def show(self):
         st.set_page_config(page_title="Nandu's CoWin api experiment")
@@ -31,9 +39,10 @@ class cowin():
         footer {visibility: hidden;}
         </style>
         """
+        df=pd.DataFrame(self.cowin_data())
+        df.rename(columns={'available_capacity_dose1':'dose1','available_capacity_dose2':'dose2'},inplace=True)
         st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-        st.write(pd.DataFrame({
-            'first column': [1, 2, 3, 4],
-            'second column': [10, 20, 30, 40] }))
+        st.dataframe(df)
 if __name__=='__main__':
     cowin().show()
+    

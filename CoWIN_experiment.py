582da105320596a468205318aca43f4ed6984cbb
Nandu  s <74356710+Nandusasikumar1@users.noreply.github.com>
2021-06-08 14:27:13 +0530

    Update CoWIN_experiment.py

--- CoWIN_experiment.py
+++ CoWIN_experiment.py
@@ -10,35 +10,38 @@ class cowin():
         return file1
     def cowin_data(self):
         file=self.read()
-        date=st.text_input('Enter date(mm-dd-yyyy)',value='01-06-2021',key=2)
+        date=st.text_input('Enter date(mm-dd-yyyy)',value='08-06-2021',key=2)
         districts=st.selectbox('Select District',list(file['districts']))
         dist_id=list(file[file['districts']==districts]['district_id'])[0]
         response=requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?',params={'district_id':str(dist_id),'date':date})
-        c=[]
-        data={}
-        details={}
+       
+        try:
+            c=[]
+            data={}
+            details={}
+            for i in response.json():
+                for k in response.json()[i]:
+                    c.append(k)
+            keyss=[i.keys() for i in c]
+            keys1=list(np.unique(keyss)[0])
+            index=[keys1.index(i) for i in ['name','pincode','fee_type','fee','min_age_limit','vaccine','available_capacity_dose1','available_capacity_dose2']]
+            
+            for f in index:
+                data[keys1[f]]=[]
+                for p in c:
+                    data[keys1[f]].append(list(p.values())[f])
+            centre=st.selectbox('Select  Vaccination Centre',data['name'])
+            
+            details['fee_type']=data['fee_type'][data['name'].index(centre)]
+            details['fee']=data['fee'][data['name'].index(centre)]
+            details['min_age_limit']=data['min_age_limit'][data['name'].index(centre)]
+            details['vaccine']=data['vaccine'][data['name'].index(centre)]
+            details['dose1']=data['available_capacity_dose1'][data['name'].index(centre)]
+            details['dose2']=data['available_capacity_dose2'][data['name'].index(centre)]
+            st.write(details)
+        except:
+            st.write('No center available')
         
-        for i in response.json():
-            for k in response.json()[i]:
-                c.append(k)
-        keyss=[i.keys() for i in c]
-        keys1=list(np.unique(keyss)[0])
-        index=[keys1.index(i) for i in ['name','pincode','fee_type','fee','min_age_limit','vaccine','available_capacity_dose1','available_capacity_dose2']]
-
-        for f in index:
-            data[keys1[f]]=[]
-            for p in c:
-                data[keys1[f]].append(list(p.values())[f])
-        centre=st.selectbox('Select  Vaccination Centre',data['name'])
-
-        details['fee_type']=data['fee_type'][data['name'].index(centre)]
-        details['fee']=data['fee'][data['name'].index(centre)]
-        details['min_age_limit']=data['min_age_limit'][data['name'].index(centre)]
-        details['vaccine']=data['vaccine'][data['name'].index(centre)]
-        details['dose1']=data['available_capacity_dose1'][data['name'].index(centre)]
-        details['dose2']=data['available_capacity_dose2'][data['name'].index(centre)]
-        
-        return details
 
     def show(self):
         st.set_page_config(page_title="Nandu's CoWin api experiment")

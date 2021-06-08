6572d155fea569802f0ce7ad8843b505c02d33c5
Nandu  s <74356710+Nandusasikumar1@users.noreply.github.com>
2021-06-08 14:22:21 +0530

    Update CoWIN_experiment.py

--- CoWIN_experiment.py
+++ CoWIN_experiment.py
@@ -17,28 +17,27 @@ class cowin():
         c=[]
         data={}
         details={}
-        try:
-            for i in response.json():
-                for k in response.json()[i]:
-                    c.append(k)
-            keyss=[i.keys() for i in c]
-            keys1=list(np.unique(keyss)[0])
-            index=[keys1.index(i) for i in ['name','pincode','fee_type','fee','min_age_limit','vaccine','available_capacity_dose1','available_capacity_dose2']]
-            
-            for f in index:
-                data[keys1[f]]=[]
-                for p in c:
-                    data[keys1[f]].append(list(p.values())[f])
-            centre=st.selectbox('Select  Vaccination Centre',data['name'])
-            
-            details['fee_type']=data['fee_type'][data['name'].index(centre)]
-            details['fee']=data['fee'][data['name'].index(centre)]
-            details['min_age_limit']=data['min_age_limit'][data['name'].index(centre)]
-            details['vaccine']=data['vaccine'][data['name'].index(centre)]
-            details['dose1']=data['available_capacity_dose1'][data['name'].index(centre)]
-            details['dose2']=data['available_capacity_dose2'][data['name'].index(centre)]
-        except:
-            st.write('No center available')
+        
+        for i in response.json():
+            for k in response.json()[i]:
+                c.append(k)
+        keyss=[i.keys() for i in c]
+        keys1=list(np.unique(keyss)[0])
+        index=[keys1.index(i) for i in ['name','pincode','fee_type','fee','min_age_limit','vaccine','available_capacity_dose1','available_capacity_dose2']]
+
+        for f in index:
+            data[keys1[f]]=[]
+            for p in c:
+                data[keys1[f]].append(list(p.values())[f])
+        centre=st.selectbox('Select  Vaccination Centre',data['name'])
+
+        details['fee_type']=data['fee_type'][data['name'].index(centre)]
+        details['fee']=data['fee'][data['name'].index(centre)]
+        details['min_age_limit']=data['min_age_limit'][data['name'].index(centre)]
+        details['vaccine']=data['vaccine'][data['name'].index(centre)]
+        details['dose1']=data['available_capacity_dose1'][data['name'].index(centre)]
+        details['dose2']=data['available_capacity_dose2'][data['name'].index(centre)]
+        
         return details
 
     def show(self):
